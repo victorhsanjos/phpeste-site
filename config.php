d6f08bb771f46c9247ea435a748f4bcb88609aa2
Victor Hugo <victorhsanjos@gmail.com>
2019-05-02 13:27:48 +0000

    Adicionando o email de contato

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'event_ticket' => null,
     'event_location' => 'Recife',
     'event_period' => 'April 17-21, 2019',
-    'event_contact_email' => 'info@yoursite.com',
+    'event_contact_email' => 'contato@phpeste.net',
     'event_facebook' => '',
     'event_twitter' => '',
     'event_instagram' => '',
