d7c171b9b9ab6379474792b91e9ce4666e2b1f8d
Fabio Fernandes <fabio.fernandes@idealizza.com.br>
2019-08-28 13:17:07 -0300

    Correção no layout do modal

--- source/_layouts/simple-page.blade.php
+++ source/_layouts/simple-page.blade.php
@@ -1,28 +1,28 @@
 @extends('_layouts.master')
 
 @section('content')
-    <div class="site-blocks-cover overlay" style="background-image: url(/assets/images/img_3.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-md-10 text-center" data-aos="fade">
-            <h1 class="mb-4">{{ $page->title }}</h1>
-          </div>
+  <div class="site-blocks-cover overlay" style="background-image: url(/assets/images/img_3.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-md-10 text-center" data-aos="fade">
+          <h1 class="mb-4">{{ $page->title }}</h1>
         </div>
       </div>
     </div>
+  </div>
 
-    <div class="site-section bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 col-lg-12 mb-5">
-            @yield('pageContent')
-          </div>
+  <div class="site-section bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-12 mb-5">
+          @yield('pageContent')
         </div>
       </div>
     </div>
+  </div>
 
     <!-- Modal -->  
-  @foreach($programming as $speaker)
+  @foreach($schedules as $speaker)
     <div class="modal right fade" id="{{$speaker->modal}}" tabindex="-1" role="dialog" aria-labelledby="{{$speaker->modal}}-speaker">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -44,7 +44,5 @@
     </div><!-- modal -->
   @endforeach
 
-    {{-- @include('_shared.why-us') --}}
-
-    @include('_shared.contact-info')
+  @include('_shared.contact-info')
 @endsection
