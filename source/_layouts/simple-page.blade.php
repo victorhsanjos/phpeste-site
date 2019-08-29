ecbc38a2901ec09cde099cc7cf5291146ef6e5e1
Filho <mizael.pedro.d.filho@accenture.com>
2019-08-29 15:59:24 -0300

    feat: Added sidebar modal
    Sidebar modal with the details of the speakers

--- source/_layouts/simple-page.blade.php
+++ source/_layouts/simple-page.blade.php
@@ -21,6 +21,30 @@
       </div>
     </div>
 
+        <!-- Modal -->  
+  @foreach($schedules as $speaker)
+  
+  <div class="modal right fade" id="{{$speaker->modal}}" tabindex="-1" role="dialog" aria-labelledby="{{$speaker->modal}}-speaker">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+
+        <div class="modal-header">
+          <h4 class="modal-title header-text" id="{{$speaker->modal}}-speaker">{{$speaker->speaker}}</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <h3 class="modal-title modal-title-text">{{$speaker->title}}</h3>
+        
+        <div class="modal-body">
+          <p>
+              {{$speaker->section}}
+          </p>
+        </div>
+
+      </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+  </div><!-- modal -->
+@endforeach
+
     {{-- @include('_shared.why-us') --}}
 
     @include('_shared.contact-info')
