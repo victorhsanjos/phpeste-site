e4bddf891d1e67f60dfc2ba05d5ed136ae60a71d
Filho <mizael.pedro.d.filho@accenture.com>
2019-08-30 15:03:58 -0300

    added sidebar modal

---
+++ source/_shared/schedules.blade.php
@@ -0,0 +1,217 @@
+<div class="site-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto text-center mb-5 section-heading">
+                <h3 class="mb-5 text-uppercase">{{ $title }}</h3>
+            </div>
+        </div>
+        <div class="row">
+            <table class="table table-bordered table-hover table-condensed text-center">
+                <thead>
+                    <tr class="thead-dark">
+                        <th scope="col" width="100"></th>
+                        <th scope="col" colspan="2">SÁBADO</th>
+                    </tr>
+                    <tr class="thead-dark">
+                        <th scope="col"></th>
+                        <th scope="col" width="44%">Trilha 1</th>
+                        <th scope="col">Trilha 2</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row" class="table-secondary">08:00</th>
+                        <td colspan="2"><strong>Credênciamento</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">08:30</th>
+                        <td colspan="2"><strong>Abertura</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">08:45</th>
+                        <td>
+                        <a href="#" data-toggle="modal" data-target="#aline">
+                        <h5>Aline Chaves</h5>
+                        <img src="https://phpeste.net/assets/images/uploads/aline-chaves.jpg" class="img-reponsive rounded-circle" width="100"><br>
+                        <small>(PHP com Rapadura e PHPWomen)</small><br>
+                        Em DDD, domain, que falta eu sinto de você
+                        </a>
+                        </td>
+                        <td>
+                        <h5>Thiago Barradas</h5>
+                        <img src="https://phpeste.net/assets/images/uploads/thiago-barradas.jpg" class="img-reponsive rounded-circle" width="100"><br>
+                        <small>(Mundipagg)</small><br>
+                        Elasticsearch - O motor de busca que está mudando o mercado!
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">09:25</th>
+                        <td>
+                        <h5>Kilderson Sena</h5>
+                        <img src="https://phpeste.net/assets/images/uploads/kilderson-sena.jpg" class="img-reponsive rounded-circle" width="100"><br>
+                        <small>(Comunidade PHP com Rapadura)</small><br>
+                        Eu sei que POO é a pedra do teu sapato
+                        </td>
+                        <td>
+                        <h5>Carol Rodrigues</h5>
+                        <img src="https://phpeste.net/assets/images/uploads/carol-rodrigues.jpg" class="img-reponsive rounded-circle" width="100"><br>
+                        <small>(PHPWomen Ceará)</small><br>
+                        Teste Gray box - Aplicação do melhor dos dois mundos
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">10:05</th>
+                        <td colspan="3"><strong>Coffee break</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">10:35</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">11:15</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">11:55</th>
+                        <td colspan="3"><strong>Intervalo</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">13:20</th>
+                        <td colspan="2">Momento "vamos que vamos"</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">13:30</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">14:10</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">14:50</th>
+                        <td colspan="3"><strong>Coffee break</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">15:30</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">16:10</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">16:50</th>
+                        <td colspan="3"><strong>Sorteio de brindes e encerramento primeiro dia</strong></td>
+                    </tr>
+                    </tbody>
+            </table>
+            <table class="table table-bordered text-center">
+                <thead>
+                    <tr class="thead-dark">
+                        <th scope="col" width="100"></th>
+                        <th scope="col" colspan="2">SÁBADO</th>
+                    </tr>
+                    <tr class="thead-dark">
+                        <th scope="col"></th>
+                        <th scope="col" width="44%">Trilha 1</th>
+                        <th scope="col">Trilha 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row" class="table-secondary">08:30</th>
+                        <td colspan="2"><strong>Abertura</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">08:40</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">09:25</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">10:05</th>
+                        <td colspan="3"><strong>Coffee break</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">10:35</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">11:15</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">11:55</th>
+                        <td colspan="3"><strong>Intervalo</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">13:20</th>
+                        <td colspan="2">Momento "vamos que vamos"</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">13:30</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">14:10</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">14:50</th>
+                        <td colspan="3"><strong>Coffee break</strong></td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">15:30</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">16:10</th>
+                        <td>Palestra</td>
+                        <td>Palestra</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="table-secondary">16:50</th>
+                        <td colspan="3"><strong>Sorteio de brindes e encerramento da conferência</strong></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- Modal -->  
+        @foreach($schedules as $speaker)
+            <div class="modal right fade" id="{{$speaker->modal}}" tabindex="-1" role="dialog" aria-labelledby="{{$speaker->modal}}-speaker">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
+
+                    <div class="modal-header">
+                    <h4 class="modal-title header-text" id="{{$speaker->modal}}-speaker">{{$speaker->speaker}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    </div>
+                    <h3 class="modal-title modal-title-text">{{$speaker->title}}</h3>
+                    
+                    <div class="modal-body">
+                    <p>
+                        {{$speaker->section}}
+                    </p>
+                    </div>
+
+                </div><!-- modal-content -->
+                </div><!-- modal-dialog -->
+            </div><!-- modal -->
+        @endforeach
+    </div>
+</div>
