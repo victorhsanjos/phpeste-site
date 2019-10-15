dc1c282dc069ea67baa656afc63f7cb1774658ab
Fabio Fernandes <fabio.fernandes@idealizza.com.br>
2019-10-15 10:19:56 -0300

    Update programing page

--- source/_shared/programming.blade.php
+++ source/_shared/programming.blade.php
@@ -15,7 +15,7 @@
             <tbody>
                 <tr>
                     <th scope="row" class="table-secondary">08:00</th>
-                    <td colspan="2"><strong>Credênciamento</strong></td>
+                    <td colspan="2"><strong>Credenciamento</strong></td>
                 </tr>
                 <tr>
                     <th scope="row" class="table-secondary">08:30</th>
